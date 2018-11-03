5bff60fb4bad46ddd9e1e821933495ab6bd45583
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-11-03 13:24:12 -0300

    add routes and components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <h1>Mario Plan</h1>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={ Dashboard }/>
+            <Route path='/project/:id' component={ ProjectDetails }/>
+            <Route path='/signin' component={ SignIn }/>
+          </Switch>
         </div>
       </BrowserRouter>
     );
