42b939d6c656a7d8a77da7c30a820bb0669e23d3
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-11-03 13:23:50 -0300

    create projects components

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by The Net Ninjas</p>
+        <p className="gray-text">3rd Septemberm, 2am</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectSummary;
